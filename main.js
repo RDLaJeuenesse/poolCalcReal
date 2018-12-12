8860a673419167a24384f8801b03a826c3994184
Richard <rdlajeuenesse76@gmail.com>
2018-12-12 15:18:41 -0700

    Pared down to one dose calculation

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const poolVolume = 26000;
 const galsPerCubicFoot = 7.5;
 const divGalsBy = 10000;
 const poolFactor = poolVolume/divGalsBy;
-let currentLevel = "okay";
 //perfect test results
 const idealResults = {
     'freeChlorine': 3,
@@ -12,7 +11,7 @@ const idealResults = {
     'pH': 7.4,
     'totalAlkalinity': 110,
     'calciumHardness': 300,
-    'cyanuricAcid': 40
+    'cyanuricAcid': 50
 };
 //chemicals and amounts to change water chem by the given ppm
 const chemicalDosageGuide = [
@@ -82,23 +81,21 @@ function round(value, decimals) {
 }
 //results array is global so we can see all results without accessing the actual test functions
 let resultsArray = [];
-console.log("Chemical Dosage Guide: ");
-console.log(chemicalDosageGuide);
 
 
+/**
+ * Standard chemical dosage equation
+ * *****INCREASE CHEMICAL
+ * PoolFactor*((ideal - current) / ppmChange)*amountNeeded
+ * 
+ */
 
-
-function chemAmount(a,b){
-
-    console.log(a.name);
-    console.log(b);
+ //chemAmount takes current, ideal, and chemDosageGuide[chemical]
+function chemAmount(a, b, c){
+    let dosage = Math.abs(round(poolFactor*((b - a) / c.ppmChange)*c.amountNeeded,2));
+    console.log(c.name);
+    console.log(dosage);
 }
-
-
-
-//Equation for amount of chemical to add
-//let chemChange = idealResults.freeChlorine - a;
-//let addAmountLow = round((poolFactor*(chemChange/chemicalDosageGuide[0].ppmChange)*chemicalDosageGuide[0].amountNeeded),2);
 
 //determines combined chlorine and determines the need for breakPoint()
 function chlorineTest(a, b) {
@@ -106,22 +103,23 @@ function chlorineTest(a, b) {
     let total = Number(b.value);
     let combined = round(total - free, 2);
 
-    combined > 0 ? breakPoint(combined, free) : console.log('Need to determine high or low.');
-    console.log("it continued");
-    /* if(free < 3 && combined === 0){
-        chemAmount(chemicalDosageGuide[0],free);
-    }else if(free > 3 && combined === 0){
-        chemAmount(chemicalDosageGuide[6], free);
-    }else if(combined > 0){
-        console.log("You have nasty chlorimines in the water. Time for a breakpoint chlorination!");
-        breakPoint(combined, free);
+    if(combined < 0){
+        console.log("RETEST!");
     }
-    else{
-        console.log('The test is invalid! Please retest.');
-    } */
+    combined > 0 ? breakPoint(combined, free) : chlorineCorrection(free);
+}
+
+function chlorineCorrection(free){
+    if(free < idealResults.freeChlorine){
+        chemAmount(free, idealResults.freeChlorine, chemicalDosageGuide[0]);
+    }else{
+        chemAmount(free, idealResults.freeChlorine, chemicalDosageGuide[6]);
+    };
+    return (chemAmount);
 }
 //calculates the amount of chemical need for breakpoint chlorination
 function breakPoint(a, b) {
+    console.log("Nasty, breakpoint needed!");
     let breakPointChlorination = a * 10;
     let chemChange = round(breakPointChlorination - b, 2);
     let addAmountBreakpoint = round((chemicalDosageGuide[0].amountNeeded * poolFactor* chemChange),2);
@@ -132,28 +130,49 @@ function breakPoint(a, b) {
 function pH(a) {
     let pH = a.value;
     if (pH <= 7.6 && pH >= 7.2) {
-        console.log("The pH is fine: "+ pH);
+        console.log("pH is within range!");
     }
     else if(pH < 7.2){
-        console.log('pH is low '+a.value);
-        chemAmount(chemicalDosageGuide[3], pH);
+        chemAmount(pH, idealResults.pH, chemicalDosageGuide[3]);
     }else{
         console.log("pH is high: "+pH);
-        chemAmount(chemicalDosageGuide[2],pH);
-        /* let addAmount = round(poolFactor*(( a.value - idealResults.pH)/chemicalDosageGuide.muriaticAcid.ppmChange)*chemicalDosageGuide.muriaticAcid.amountNeeded,2);
-        console.log("Add amount: "+addAmount); */
+        chemAmount(pH, idealResults.pH, chemicalDosageGuide[2]);
     }
 }
-
-
-
-
-
-
-
-
-
-
+function totalAlkalinity(a) {
+    let alk = a.value;
+    if (alk <= 120 && alk >= 80) {
+        console.log("Alkalinity is within range!");
+    }
+    else if(alk < 80){
+        chemAmount(alk, idealResults.totalAlkalinity, chemicalDosageGuide[1]);
+    }else{
+        console.log("Alkalinity is high: "+alk);
+        chemAmount(alk, idealResults.alkalinity, chemicalDosageGuide[2]);
+    }
+}
+function cyanuricAcid(a) {
+    let cyanuric = a.value;
+    if (cyanuric <= 100 && cyanuric >= 50) {
+        console.log("Cyanuric acid is within range!");
+    }
+    else if(cyanuric < 100){
+        chemAmount(cyanuric, idealResults.cyanuricAcid, chemicalDosageGuide[5]);
+    }else{
+        console.log("Cyanuric is high drain two feet of water and refill.");
+    }
+}
+function calciumHardness(a) {
+    let calcium = a.value;
+    if (calcium <= 400 && calcium >= 150) {
+        console.log("Calcium acid is within range!");
+    }
+    else if(calcium < 150){
+        chemAmount(calcium, idealResults.calciumHardness, chemicalDosageGuide[4]);
+    }else{
+        console.log("Calcium is high drain two feet of water and refill.");
+    }
+}
 
 //runs all the associated chemical tests or issues an error this is called by pushing the results button
 function runTests() {
@@ -163,9 +182,9 @@ function runTests() {
     chlorineTest(testInputs[0], testInputs[1]);
     //testInputs[2] is the calculated combinedChlorine
     pH(testInputs[3]);
-/*totalAlkalinity(testInputs[4]);
+    totalAlkalinity(testInputs[4]);
     cyanuricAcid(testInputs[5]);
-    calciumHardness(testInputs[6]); */
+    calciumHardness(testInputs[6]);
     //resultsMessage(testInputs);
 }
 //this event listener updates the cobined chlorine total before the inputs array has been created
@@ -175,14 +194,3 @@ document.getElementById('total').addEventListener('blur', function () {
 btnResults.addEventListener('click', function () {
     runTests();
 })
-/* let pickle = chemicalDosageGuide[0];
-console.log(pickle);
-console.log(typeof(pickle));
-console.log(pickle.name); */
-
-/**
- * Standard chemical dosage equation
- * *****INCREASE CHEMICAL
- * PoolFactor*((ideal - current) / ppmChange)*amountNeeded
- * 
- */
